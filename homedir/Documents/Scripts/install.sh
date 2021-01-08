0f2de8bc482b0f290b42afff287a206af4197798
Antoine Carnec <antoinecarnec@gmail.com>
2021-01-08 16:42:40 +0000

    Added install script

---
+++ homedir/Documents/Scripts/install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+curl -L https://nixos.org/nix/install | sh
+
+. ${HOME}/.nix-profile/etc/profile.d/nix.sh
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+
+nix-channel --update
+
+export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+
+nix-shell '<home-manager>' -A install
+
+. ${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh
+
+printf "\n\n\n"
+echo "Please run home-manager switch after importing the git repository"
