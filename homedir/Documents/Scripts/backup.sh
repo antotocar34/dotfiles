351c70310c5643e87dfb033ac9a87fe953aaf900
Antoine Carnec <antoinecarnec@gmail.com>
2021-01-28 14:53:05 +0000

    Got backup.sh working

--- homedir/Documents/Scripts/backup.sh
+++ homedir/Documents/Scripts/backup.sh
@@ -1,15 +1,11 @@
 #! /bin/bash
 
-# include_list="--include ${HOME}/Documents --include ${HOME}/Music --include ${HOME}/Pictures --include ${HOME}/Videos" 
-
-# rsync -v -avzhe ssh /home/carneca/ --include-from=include.txt acer:/home/carnec/SystemBackups
-
 rclone_backup () {
-${HOME}/.nix-profile/bin/rclone -v copy ${HOME}/$1 tdrive:/backup/$1/
+${HOME}/.nix-profile/bin/rclone -v copy ${HOME}/Documents tdrive:/backup/documents/
 }
 
-rclone_backup "Documents"
+rclone_backup 2> /home/carneca/log
 while [ $? -ne 0 ] ; do
-    rclone_backup "Documents"
-    sleep 4
+    rclone_backup 2> /home/carneca/.log/rclone_backup
+    sleep 3
 done
