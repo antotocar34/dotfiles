39bd750e1cc78cc9231d9ce41871e15eadee1baa
Antoine Carnec <antoinecarnec@gmail.com>
2021-03-28 19:15:07 +0100

    Added taskwarrior backups

--- homedir/Documents/Scripts/weekly_backup.sh
+++ homedir/Documents/Scripts/weekly_backup.sh
@@ -2,7 +2,7 @@
 #! /bin/bash
 
 rclone_sync () {
-${HOME}/.nix-profile/bin/rclone -v sync ${HOME}/Documents tdrive:/backup/Documents/
+# ${HOME}/.nix-profile/bin/rclone -v sync ${HOME}/Documents tdrive:/backup/Documents/
 ${HOME}/.nix-profile/bin/rclone -v sync ${HOME}/Music tdrive:/backup/Music
 }
 
