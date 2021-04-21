ba5f8352819579cf83aca60968c9e38a9d61c58b
Antoine Carnec <antoinecarnec@gmail.com>
2021-04-21 18:18:23 +0100

    update

--- homedir/Documents/Scripts/find_pdf.sh
+++ homedir/Documents/Scripts/find_pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Calls rofi on find and doesn't return anything if cancelled
 
-FILE=$(fd -I -epdf . ~/Documents/ ~/Downloads/ ~/Documents/Latex  | rofi -dmenu -i)
+FILE=$(fd -uu -epdf . ~/Documents/ ~/Downloads/ ~/Documents/Latex  | rofi -dmenu -i)
 
 if [[ ${?} -eq 0 ]]
 then
