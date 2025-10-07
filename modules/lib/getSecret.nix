8a691e5fd1fa601f5ed5b27ab6b9244c1e1a2382
Antoine Carnec <antoinecarnec@gmail.com>
2025-10-07 16:02:07 +0100

    Refactor: Remove redundant `getSecrets.nix` and simplify `getSecret` warning message.

--- modules/lib/getSecret.nix
+++ modules/lib/getSecret.nix
@@ -5,6 +5,6 @@
   secret: 
   lib.attrsets.attrByPath 
     [ "age" "secrets" "${secret}" "path" ]
-    (lib.warn "The secret '${secret}' was not accessible ${config.age}" "/dev/null") 
+    (lib.warn "The secret '${secret}' was not accessible" "/dev/null") 
     config;
 }
